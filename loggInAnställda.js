1d1122da1e8edf3cc36d0a79ba3af7f624735d67
Miwa G <miwa.guhres@hotmail.com>
2024-04-24 21:31:09 +0200

    save logged-in user in localStrage

---
+++ loggInAnställda.js
@@ -0,0 +1,47 @@
+fetch('mockData/anställda.json')
+  .then(response => response.json())
+  .then(data => {
+
+const isvalidUser= (email, password)=>{
+    console.log(inputEmail)
+    console.log(inputPassword)
+
+    const findedUser= data.find( u => u.email === email && u.password === password);
+    console.log(findedUser)
+
+
+    if(findedUser ){
+
+        
+        if(findedUser.role==="admin"){
+            alert("Välkommen "+ findedUser.firstname+ " som admin.");
+            console.log("Login successful.Redirecting to admin.html")
+            localStorage.setItem('logged-inUserData', JSON.stringify(findedUser));
+            window.location.href = "admin.html";
+        } else{
+            alert("Välkommen "+ findedUser.firstname+ " som städare.");
+            console.log("Login successful.Redirecting to städare.html")
+            window.location.href = "städare.html"; 
+        }
+    } else {
+        alert("Ogiltig e-postadress eller ogiltigt lösenord. Vänligen försök igen")
+        console.log("Invaid email eller password")
+        
+    }
+    
+} 
+
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const inputEmail = document.getElementById('inputEmail').value;
+    const inputPassword = document.getElementById('inputPassword').value;
+    isvalidUser(inputEmail, inputPassword);
+  });
+
+
+})
+  .catch(error => console.error('Error fetching customer data:', error));
+
+
+
+
---
+++ loggInPrivat.js
@@ -0,0 +1,47 @@
+fetch('mockData/kunder_privat.json')
+  .then(response => response.json())
+  .then(data => {
+
+const isvalidUser= (email, password)=>{
+    console.log(inputEmail)
+    console.log(inputPassword)
+
+    const findedUser= data.find( u => u.email === email && u.password === password);
+    console.log(findedUser)
+
+
+    if(findedUser ){
+
+        
+        if(findedUser.role==="private"){
+            alert("Välkommen "+ findedUser.firstname+ " som privatkund.");
+            console.log("Login successful.Redirecting to privatkund.html")
+            localStorage.setItem('logged-inUserData', JSON.stringify(findedUser));
+            window.location.href = "privatkund.html";
+        } else{
+            alert("Välkommen "+ findedUser.firstname+ " som företagkund.");
+            console.log("Login successful.Redirecting to företagskund.html")
+            window.location.href = "företagskund.html"; 
+        }
+    } else {
+        alert("Ogiltig e-postadress eller ogiltigt lösenord. Vänligen försök igen")
+        console.log("Invaid email eller password")
+        
+    }
+    
+} 
+
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const inputEmail = document.getElementById('inputEmail').value;
+    const inputPassword = document.getElementById('inputPassword').value;
+    isvalidUser(inputEmail, inputPassword);
+  });
+
+
+})
+  .catch(error => console.error('Error fetching customer data:', error));
+
+
+
+
